84acf0ee3bd305d28f53e7c2eda66fa4255c09f5
JeSha55 <MenesesSebas05@gmail.com>
2023-03-29 12:53:44 -0500

    Se hizo eñ primer comentario

---
+++ index.js
@@ -0,0 +1,6 @@
+const navtoggle =   document.querySelector(".nav-toggle");
+const navmenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click" , () => {
+    navMenu.classList.toggle("nav-menu_visible");
+});
